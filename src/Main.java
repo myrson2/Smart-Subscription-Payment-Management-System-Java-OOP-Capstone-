4007e8040808bdabe714434e89b57af3e5c49ca5
Myrson2 <beros.josemyrson.oreo1@gmail.com>
2026-01-04 23:56:25 +0800

    Initial Log in v1.1

--- src/Main.java
+++ src/Main.java
@@ -3,56 +3,59 @@ import Users.Customer;
 import Users.Admin;
 
 public class Main {
+    
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        Customer customer = null;
-        Admin admin = null;
-        
-        System.out.println("Smart Subscription & Payment Management System\n");
-
-        System.out.println("==============================================");
-        System.out.println("Login Form: \n");
-
-        // System User Login 
-        int id;
-        String name,
-            email,
-            password;
-
-        do{
-            System.out.println("User ID: ");
-            id = scan.nextInt();
-            scan.nextLine();
-
-            System.out.println("Name: ");
-            name = scan.nextLine();
-
-            System.out.println("Email: ");
-            email = scan.nextLine();
-
-            System.out.println("Password: ");
-            password = scan.nextLine();
+        try (Scanner scan = new Scanner(System.in)) {
+            Customer customer = null;
+            Admin admin = null;
+            
+            System.out.println("Smart Subscription & Payment Management System\n");
 
             System.out.println("==============================================");
-            System.out.println("1 - Customer || 2 - Admin");
+            System.out.println("Login Form: \n");
 
-            int choice = 0;
-            System.out.println("Enter Choice: ");
-            choice = scan.nextInt();
-            scan.nextLine();
+            // System User Login 
+            int id;
+            String name,
+                email,
+                password;
 
-            if(choice == 1){
-                customer = new Customer(id, name, email, password, true);
-            } else if (choice == 2){
-                admin = new Admin(id, name, email, password, true);
-            }
-            scan.close();
+            do{
+                System.out.println("User ID: ");
+                id = scan.nextInt();
+                scan.nextLine();
 
-        } while (true);
-        
+                System.out.println("Name: ");
+                name = scan.nextLine();
+
+                System.out.println("Email: ");
+                email = scan.nextLine();
+
+                System.out.println("Password: ");
+                password = scan.nextLine();
+
+                System.out.println("==============================================");
+                System.out.println("1 - Customer || 2 - Admin");
+
+                int choice = 0;
+                System.out.println("Enter Choice: ");
+                choice = scan.nextInt();
+                scan.nextLine();
+
+                switch (choice) {
+                    case 1:
+                        customer = new Customer(id, name, email, password, true);
+                        break;
+                    case 2:
+                        admin = new Admin(id, name, email, password, true);                        break;
+                    default:
+                        break;
+                }
+            } while (true);
+        }    
+    
     }
 
     // function for admin dashboard
-
     // function for customer dashboard
 }
--- src/Users/Admin.java
+++ src/Users/Admin.java
@@ -5,4 +5,8 @@ public class Admin extends User{
     public Admin(int userId, String name, String email, String password, boolean isActive){
         super(userId, name, email, password, isActive);
     }
+
+    public void createSubscriptionPlan(){
+        
+    }
 }
